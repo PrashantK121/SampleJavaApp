3b8ffb2bef06b015b5927a5b51b925ebfe8363a8
prashantkushwaha121 <prashantkushwaha121@gmail.com>
2025-01-22 21:42:34 +0530

    kafka-batch

--- HelloWorld/src/main/java/com/example/controller/BatchController.java
+++ HelloWorld/src/main/java/com/example/controller/BatchController.java
@@ -4,10 +4,16 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
+import java.util.concurrent.CompletableFuture;
+
+@EnableAsync
 @RestController
 @RequestMapping(path = "/v1")
 public class BatchController {
@@ -18,14 +24,17 @@ public class BatchController {
     @Autowired
     private Job jobSendMessage;
 
+    @Async
     @GetMapping("/run-batch")
-    public String runBatch() {
-        try {
-            jobLauncher.run(jobSendMessage, new JobParametersBuilder().addLong("time", System.currentTimeMillis()).toJobParameters());
-            return "Batch job has been invoked";
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Batch job failed to start";
-        }
+    public CompletableFuture<ResponseEntity<String>> runBatch() {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                jobLauncher.run(jobSendMessage, new JobParametersBuilder().addLong("time", System.currentTimeMillis()).toJobParameters());
+                return ResponseEntity.ok("Batch job has been invoked");
+            } catch (Exception e) {
+                e.printStackTrace();
+                return ResponseEntity.status(500).body("Batch job failed to start");
+            }
+        });
     }
 }
--- HelloWorld/src/main/java/com/example/steps/writer/UserItemWriter.java
+++ HelloWorld/src/main/java/com/example/steps/writer/UserItemWriter.java
@@ -4,7 +4,6 @@ import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.entity.User;
@@ -17,17 +16,16 @@ import java.util.stream.Collectors;
 @Component
 public class UserItemWriter implements ItemWriter<User> {
 
-	//@Autowired public EventProducer eventProducer;
+	@Autowired public EventProducer eventProducer;
 
 	@Autowired
 	UserRepository userRepository;
 	
 	@Override
-	@Transactional
 	public void write(Chunk<? extends User> users) throws Exception {
 		List<User> userList = users.getItems().stream().map(user -> {
 			System.out.println("Entered for processing : "+user.getId());
-			//eventProducer.sendKafkaMessage(user.getFirstName());
+			eventProducer.sendKafkaMessage(user.getFirstName());
 			user.setFirstName(user.getFirstName().toUpperCase() + "1");
 			//user.setId(user.getId()+10);
 			return user;
